6716310b5795d83b0a591b429132fba5c8ea36a8
Mark Batho <mark747488@gmail.com>
2023-06-07 21:24:42 +0200

    Add navbar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,36 @@
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
+import { useState } from 'react';
+
+const views = {
+  EDIT: 'edit',
+  PREVIEW: 'preview',
+};
+
 function App() {
+  const [view, setView] = useState(views.EDIT);
+
   return (
     <div className="App">
+      <Navbar className="px-2 py-0" bg="white" variant="light" fixed="top">
+        <Container fluid>
+          <Navbar.Brand href="#">CV Application</Navbar.Brand>
+          <Nav className="justify-content-end">
+            <Button
+              variant="primary"
+              onClick={() => {
+                view === views.EDIT
+                  ? setView(views.PREVIEW)
+                  : setView(views.EDIT);
+              }}
+            >
+              {view === views.EDIT ? 'Preview' : 'Edit'}
+            </Button>
+          </Nav>
+        </Container>
+      </Navbar>
       <h1>Hello, World!</h1>
     </div>
   );
