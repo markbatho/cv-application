ff9a2279f9d2ea78f8e3ec4e22c3eb8194f89c57
Mark Batho <mark747488@gmail.com>
2023-06-11 21:19:26 +0200

    Add save and edit options for General Information

--- src/components/edit/GeneralInformation.jsx
+++ src/components/edit/GeneralInformation.jsx
@@ -2,6 +2,7 @@ import Card from 'react-bootstrap/Card';
 import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
 
 export default function GeneralInformation({ generalInfo, setGeneralInfo }) {
   function handleOnChange(e) {
@@ -11,14 +12,30 @@ export default function GeneralInformation({ generalInfo, setGeneralInfo }) {
     });
   }
 
+  function handleSubmit(e) {
+    e.preventDefault();
+    setGeneralInfo({
+      ...generalInfo,
+      isValid: true,
+    });
+  }
+
+  function handleEdit() {
+    setGeneralInfo({
+      ...generalInfo,
+      isValid: false,
+    });
+  }
+
   return (
     <section className="d-flex flex-column gap-2">
       <div className="section-header">
         <h2>General Information</h2>
       </div>
       <Card>
+        <Card.Header>General Information</Card.Header>
         <Card.Body>
-          <Form>
+          <Form id="general-info-form" onSubmit={(e) => handleSubmit(e)}>
             <Row className="mb-3">
               <Form.Group as={Col} controlId="firstName">
                 <Form.Label>First Name</Form.Label>
@@ -27,6 +44,8 @@ export default function GeneralInformation({ generalInfo, setGeneralInfo }) {
                   value={generalInfo.firstName}
                   type="text"
                   placeholder="John"
+                  required
+                  disabled={generalInfo.isValid ? true : false}
                 />
               </Form.Group>
               <Form.Group as={Col} controlId="lastName">
@@ -36,6 +55,8 @@ export default function GeneralInformation({ generalInfo, setGeneralInfo }) {
                   value={generalInfo.lastName}
                   type="text"
                   placeholder="Doe"
+                  required
+                  disabled={generalInfo.isValid ? true : false}
                 />
               </Form.Group>
             </Row>
@@ -47,6 +68,8 @@ export default function GeneralInformation({ generalInfo, setGeneralInfo }) {
                   value={generalInfo.email}
                   type="email"
                   placeholder="johndoe@example.com"
+                  required
+                  disabled={generalInfo.isValid ? true : false}
                 />
               </Form.Group>
               <Form.Group as={Col} controlId="phone">
@@ -56,6 +79,8 @@ export default function GeneralInformation({ generalInfo, setGeneralInfo }) {
                   value={generalInfo.phone}
                   type="tel"
                   placeholder="+36-30-123-4567"
+                  required
+                  disabled={generalInfo.isValid ? true : false}
                 />
               </Form.Group>
             </Row>
@@ -66,6 +91,8 @@ export default function GeneralInformation({ generalInfo, setGeneralInfo }) {
                 value={generalInfo.address}
                 type="text"
                 placeholder="200 Some Street Scranton PA 12345 USA"
+                required
+                disabled={generalInfo.isValid ? true : false}
               />
             </Form.Group>
             <Form.Group className="mt-3" as={Col} controlId="about">
@@ -76,10 +103,28 @@ export default function GeneralInformation({ generalInfo, setGeneralInfo }) {
                 as="textarea"
                 rows={4}
                 placeholder="Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat."
+                required
+                disabled={generalInfo.isValid ? true : false}
               />
             </Form.Group>
           </Form>
         </Card.Body>
+        <Card.Footer className="text-end">
+          <Button
+            form="general-info-form"
+            type="submit"
+            variant="outline-success"
+          >
+            Save
+          </Button>
+          <Button
+            className="mx-2"
+            onClick={handleEdit}
+            variant="outline-warning"
+          >
+            Edit
+          </Button>
+        </Card.Footer>
       </Card>
     </section>
   );
